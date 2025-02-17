10a258c42e31acd781aff3c89bb948f08aa4355a
Michel Diz <MichelDiz@users.noreply.github.com>
2025-02-17 16:58:34 -0300

    Adiciona script de seed para criar um usuário Super Admin no banco de dados

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const superAdminEmail = "superadmin@email.com";
+    const superAdminUsername = "superadmin";
+
+    const hashedPassword = await bcrypt.hash("superadminpassword", 10);
+
+    await prisma.user.create({
+        data: {
+            name: "Super Admin",
+            email: superAdminEmail,
+            username: superAdminUsername,
+            password: hashedPassword,
+            role: "ADMIN",
+        },
+    });
+
+    console.log("✅ Super Admin criado com sucesso!");
+
+}
+
+main()
+    .catch((e) => {
+        console.error("Erro ao rodar o seed:", e);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
