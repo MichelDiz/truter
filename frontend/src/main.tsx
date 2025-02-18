1ce7976070eccfa59fc8a5598050e126b9e7a63d
Michel Diz <MichelDiz@users.noreply.github.com>
2025-02-17 21:29:39 -0300

    Adiciona suporte ao Apollo Client para gerenciamento de estado GraphQL

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloProvider } from "@apollo/client";
+import client from "./graphql/apolloClient";
 import AppRoutes from "./routes";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AppRoutes />
+    <ApolloProvider client={client}>
+      <AppRoutes />
+    </ApolloProvider>
   </React.StrictMode>
 );
