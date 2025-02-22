78e255bb3afa43887185ca9d641d6f9328b6b2bf
Michel Diz <MichelDiz@users.noreply.github.com>
2025-02-22 19:15:23 -0300

    Adiciona teste E2E para a funcionalidade de login via GraphQL

---
+++ e2e/tests/auth/login_test.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"e2e/truter/setup"
+	"testing"
+)
+
+func TestLogin(t *testing.T) {
+	endpoint := "http://backend_server:4000/graphql"
+
+	mutation := `
+		mutation Login($username: String!, $password: String!) {
+			loginUser(username: $username, password: $password) {
+				user {
+					id
+					name
+					email
+					role
+				}
+				message
+				key
+			}
+		}
+	`
+
+	variables := map[string]interface{}{
+		"username": "superadmin",
+		"password": "superadminpassword",
+	}
+
+	statusCode, body := setup.GraphQLRequest(t, endpoint, mutation, variables)
+
+	if statusCode != 200 {
+		t.Errorf("Erro ao fazer login. Esperado 200, mas recebeu %d. Resposta: %s", statusCode, body)
+	}
+}
