118740db09fae668a153bdd6c92bb503b3688ae1
Michel Diz <MichelDiz@users.noreply.github.com>
2025-02-22 16:44:02 -0300

    Remove testes E2E do Google e adiciona novos testes para verificar o Nginx e a API, além de incluir script para execução dos testes

--- e2e/main.go
+++ e2e/main.go
@@ -1,25 +1,9 @@
 package main
 
 import (
-	"context"
 	"fmt"
-
-	"github.com/chromedp/chromedp"
 )
 
 func main() {
-	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
-
-	var title string
-	err := chromedp.Run(ctx,
-		chromedp.Navigate("https://www.google.com"),
-		chromedp.Title(&title),
-	)
-	if err != nil {
-		fmt.Println("Erro:", err)
-		return
-	}
-
-	fmt.Println("Título da página:", title)
+	fmt.Println("Use o sistema de testes do Go")
 }
---
+++ runTests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker compose -f docker-compose-e2e.yml up --build --attach e2e-tests
+
+# echo "Removendo containers temporários..."
+# docker compose -f docker-compose-e2e.yml down --volumes --remove-orphans
